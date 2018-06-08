5eb019e8a3d253946a87d5ce7960b7fb5cd52240
Peter <peter@peter-debian.local>
2018-06-08 20:46:29 +0200

    add popper

--- bin/download_vendor_libs.sh
+++ bin/download_vendor_libs.sh
@@ -10,6 +10,7 @@ curl -s https://raw.githubusercontent.com/aws/amazon-cognito-identity-js/v1.16.0
 curl -s https://raw.githubusercontent.com/aws/amazon-cognito-identity-js/v1.16.0/dist/aws-cognito-sdk.min.js -o static/aws-sdks/amazon-cognito-sdk.v1.16.0.min.js 
 curl -s https://raw.githubusercontent.com/aws/aws-sdk-js/v2.36.0/dist/aws-sdk.min.js -o static/aws-sdks/aws-sdk.v2.36.0.min.js
 curl -s https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js -o static/js/jquery-1.12.4.min.js
+curl -s https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js -o static/js/popper.min.js
 curl -s https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js -o static/js/bootstrap-4.1.1.min.js
 curl -s https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1//css/bootstrap.min.css -o static/css/bootstrap-4.1.1.min.css
 curl -s https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1//css/bootstrap-theme.min.css -o static/css/bootstrap-theme-4.1.1.min.css
